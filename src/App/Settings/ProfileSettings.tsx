494c00a6c6878cd60d4fbe3332af881af8802781
Masterravi2000 <raviqazqwerty123456@gmail.com>
2026-02-18 02:05:58 +0530

    react-native-image-picker installed and tested

--- src/App/Settings/ProfileSettings.tsx
+++ src/App/Settings/ProfileSettings.tsx
@@ -1,15 +1,32 @@
-import { StyleSheet, TouchableOpacity, Image, Dimensions, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, TouchableOpacity, Image, Dimensions, View, Platform, PermissionsAndroid } from 'react-native'
+import { useState } from 'react'
 import coverpic from '../../../assets/visuals/images/coverpic.jpg'
 import PageThemeView from '../../components/PageThemeView'
 import TextScallingFalse from '../../components/TextScallingFalse'
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const ProfileSettings = () => {
+    const [coverUri, setCoverUri] = useState<string | null>(null);
+
+    const openGallery = async () => {
+        if (Platform.OS == 'android') {
+            await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES
+            );
+        }
+        launchImageLibrary({ mediaType: 'photo' }, (response) => {
+            const uri = response.assets?.[0]?.uri;
+
+            if (uri) {
+                setCoverUri(uri);
+            }
+        });
+    }
     return (
         <PageThemeView>
             <View style={styles.CoverPicContainer}>
                 <TouchableOpacity activeOpacity={0.9} style={{ zIndex: 50, flexDirection: 'row', gap: 6, alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute', borderRadius: 30 }}>
-                    <TouchableOpacity activeOpacity={0.5} style={{borderWidth: 1, borderColor:'white', paddingHorizontal: 10, paddingVertical: 5, borderRadius: 100, flexDirection:'row', gap: 5}}>
+                    <TouchableOpacity onPress={openGallery} activeOpacity={0.5} style={{ borderWidth: 1, borderColor: 'white', paddingHorizontal: 10, paddingVertical: 5, borderRadius: 100, flexDirection: 'row', gap: 5 }}>
                         <TextScallingFalse style={{ color: 'white', fontSize: 13, fontWeight: '600' }}>Change Cover</TextScallingFalse>
                         <TextScallingFalse style={{ color: 'white', fontSize: 13 }}>@</TextScallingFalse>
                     </TouchableOpacity>
@@ -20,8 +37,8 @@ const ProfileSettings = () => {
                 />
                 {/* profile pic part */}
                 <View style={styles.ProfilePicContainer}>
-                    <TouchableOpacity activeOpacity={0.9} style={{width:'100%', alignItems:'center', justifyContent:'center', height:'100%', zIndex: 100, position:'absolute', borderRadius: size/2, backgroundColor:'rgb(0,0,0,0.5)'}} >
-                    <TextScallingFalse style={{ color: 'white', fontSize: 20, alignSelf:'center'}}>@</TextScallingFalse>
+                    <TouchableOpacity activeOpacity={0.9} style={{ width: '100%', alignItems: 'center', justifyContent: 'center', height: '100%', zIndex: 100, position: 'absolute', borderRadius: size / 2, backgroundColor: 'rgb(0,0,0,0.5)' }} >
+                        <TextScallingFalse style={{ color: 'white', fontSize: 20, alignSelf: 'center' }}>@</TextScallingFalse>
                     </TouchableOpacity>
                     <Image
                         source={coverpic}
