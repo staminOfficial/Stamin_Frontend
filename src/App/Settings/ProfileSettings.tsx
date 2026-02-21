20760711ba5d83b5f31bf5751ad4581371e219d7
Masterravi2000 <raviqazqwerty123456@gmail.com>
2026-02-22 02:00:35 +0530

    Profile Settings updated

--- src/App/Settings/ProfileSettings.tsx
+++ src/App/Settings/ProfileSettings.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, TouchableOpacity, Image, Dimensions, View, Platform, PermissionsAndroid } from 'react-native'
+import { StyleSheet, TouchableOpacity, Image, Dimensions, View, Platform, PermissionsAndroid, TextInput, KeyboardAvoidingView } from 'react-native'
 import { useState } from 'react'
 import coverpic from '../../../assets/visuals/images/coverpic.jpg'
 import PageThemeView from '../../components/PageThemeView'
 import TextScallingFalse from '../../components/TextScallingFalse'
-import { launchImageLibrary } from 'react-native-image-picker';
 import ImagePicker from 'react-native-image-crop-picker'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import BackButtonSvg from '../../components/Svg/Icons_svg/BackButtonSvg'
+import SmallBackButtonSvg from '../../components/Svg/Icons_svg/SmallBackButton'
 
 const ProfileSettings = () => {
     const [coverUri, setCoverUri] = useState<string | null>(null);
     const [profileUri, setProfileUri] = useState<string | null>(null);
+    const [modal, setModal] = useState(false);
+    const [modalData, setModalData] = useState<string | null>(null);
+    const [calender, setCalender] = useState(false);
+    const [date, setDate] = useState(new Date());
+
+    //for temperorly lets create userData an local state here
+    const [userData, setUserData] = useState({
+        First_Name: "Ravi",
+        Last_Name: "Sharma",
+        Date_Of_Birth: "17/06/2001",
+        location: "Brooklyn, Britain",
+        height: "6ft",
+        weight: "65kg",
+        about: "Pushing Limits on two Wheels - Elite Road Cyclist With a Passion for Speed and Endurance"
+    })
 
     const openGallery = async (type: string) => {
         try {
@@ -33,9 +50,89 @@ const ProfileSettings = () => {
         }
     };
 
+    const editingOptionsData = [
+        {
+            id: 1,
+            name: 'First Name',
+            icon: '@'
+        },
+        {
+            id: 2,
+            name: 'Last Name',
+            icon: '@'
+        },
+        {
+            id: 3,
+            name: 'Date of Birth',
+            icon: '@'
+        },
+        {
+            id: 4,
+            name: 'Location',
+            icon: '@'
+        },
+        {
+            id: 5,
+            name: 'Height',
+            icon: '@'
+        },
+        {
+            id: 6,
+            name: 'Weight',
+            icon: '@'
+        },
+        {
+            id: 7,
+            name: 'About',
+            icon: '@'
+        }
+    ]
+
+    const openModal = (name: string) => {
+        if (name === "Date of Birth") {
+            setCalender(true);
+        }
+        else {
+            setModalData(name);
+            setModal(true);
+        }
+    }
+
+    const EditingOptions = () => {
+        return editingOptionsData.map((e, i) => {
+            const keyMap: Record<string, keyof typeof userData> = {
+                'First Name': 'First_Name',
+                'Last Name': 'Last_Name',
+                'Date of Birth': 'Date_Of_Birth',
+                'Location': 'location',
+                'Height': 'height',
+                'Weight': 'weight',
+                'About': 'about'
+            };
+            const value = userData[keyMap[e.name]];
+            return (
+                <View key={i} style={styles.editInfoContainer}>
+                    <TextScallingFalse style={styles.QuestionText}>{e.name}</TextScallingFalse>
+                    {e.name === "First Name" || e.name === "Last Name" ? (
+                        <View style={styles.WritingContainer}>
+                            <TextInput numberOfLines={1} style={styles.AnswerText}>{value}</TextInput>
+                            <View style={{ alignItems: 'center', justifyContent: 'center' }}><TextScallingFalse style={{ color: 'white' }}>{e.icon}</TextScallingFalse></View>
+                        </View>
+                    ) : (
+                        <TouchableOpacity onPress={() => openModal(e.name)} activeOpacity={0.7} style={styles.WritingContainer}>
+                            <TextInput editable={false} numberOfLines={1} style={styles.AnswerText}>{value}</TextInput>
+                            <View style={{ alignItems: 'center', justifyContent: 'center' }}><TextScallingFalse style={{ color: 'white' }}>{e.icon}</TextScallingFalse></View>
+                        </TouchableOpacity>
+                    )}
+                </View>
+            )
+        })
+    }
+
+
     return (
         <PageThemeView>
-            <View style={styles.mainView}>
+            <KeyboardAvoidingView style={styles.mainView}>
                 {/* cover and profilepic section */}
                 <View style={styles.CoverPicContainer}>
                     <TouchableOpacity onPress={() => openGallery("cover")} activeOpacity={0.9} style={styles.CoverPicView}>
@@ -59,34 +156,45 @@ const ProfileSettings = () => {
                         />
                     </View>
                 </View>
-
+                {/* editting section */}
                 <View style={styles.UserInfoEditingContainer}>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>First Name   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>Ravi</TextScallingFalse>
+                    {EditingOptions()}
+                </View>
+            </KeyboardAvoidingView>
+
+            {modal === true ? (
+                <View style={{ width: '100%', height: '100%', zIndex: 100, position: 'absolute', backgroundColor: 'black', gap: 20 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 20 }}>
+                        <TouchableOpacity onPress={() => setModal(false)} style={{ paddingHorizontal: 15, paddingVertical: 10, }}>
+                            <SmallBackButtonSvg />
+                        </TouchableOpacity>
+                        <TextScallingFalse style={{ color: 'white', fontSize: 19, fontWeight: '500' }}>{modalData}</TextScallingFalse>
                     </View>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>Last Name   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>Sharma</TextScallingFalse>
-                    </View>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>Date of Birth   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>17/06/2001</TextScallingFalse>
-                    </View>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>Location   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>Brooklyn, Britain</TextScallingFalse>
-                    </View>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>Height   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>6ft</TextScallingFalse>
-                    </View>
-                    <View style={styles.editInfoContainer}>
-                        <TextScallingFalse style={styles.text}>Weight   -</TextScallingFalse>
-                        <TextScallingFalse style={styles.text}>65kg</TextScallingFalse>
+                    <View style={{width:'100%', paddingHorizontal: 20}}>
+                        <TextScallingFalse style={styles.AnswerText}>Brooklyn, Britain</TextScallingFalse>
+                        <View style={{borderBottomWidth: 1, borderColor:'white', width:'100%'}} />
                     </View>
                 </View>
-            </View>
+            ) : null}
+            {/* Date Of Birth Input */}
+            {calender && (
+                <DateTimePicker
+                    value={date}
+                    mode="date"
+                    display="default"
+                    onChange={(event, selectedDate) => {
+                        setCalender(false);
+                        if (selectedDate) {
+                            const formatted = selectedDate.toLocaleDateString("en-GB");
+
+                            setUserData(prev => ({
+                                ...prev,
+                                Date_Of_Birth: formatted
+                            }));
+                        }
+                    }}
+                />
+            )}
         </PageThemeView>
     )
 }
@@ -99,7 +207,7 @@ const styles = StyleSheet.create({
         width: '100%',
         paddingVertical: 20,
         paddingHorizontal: 4
-    }, 
+    },
     CoverPicContainer: {
         width: '100%',
         height: 190,
@@ -172,25 +280,40 @@ const styles = StyleSheet.create({
         borderRadius: size / 2,
         backgroundColor: 'rgb(0,0,0,0.5)'
     },
-    UserInfoEditingContainer:{
-        width: '100%', 
-        paddingHorizontal: 10, 
-        paddingVertical: 10, 
+    UserInfoEditingContainer: {
+        width: '100%',
+        paddingHorizontal: 20,
+        paddingRight: 25,
+        paddingVertical: 10,
         gap: 10
     },
-    editInfoContainer:{
-         flexDirection: 'row', 
-         gap: 10,
-         width:'100%',
-         paddingVertical: 10,
-         paddingHorizontal: 20,
-         borderRadius: 10,
-         borderWidth: 1,
-         borderColor:'#252525',
+    editInfoContainer: {
+        flexDirection: 'row',
+        gap: 10,
+        width: '100%',
+        borderRadius: 10,
+        alignItems: 'center'
     },
-    text:{
-        color: 'white', 
-        fontSize: 17, 
-        fontWeight: '400'
+    QuestionText: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: '400',
+        width: 110
+    },
+    AnswerText: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: '400',
+        width: '74%',
+    },
+    AnswerTextAbout: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: '400',
+    },
+    WritingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: "65%"
     }
 })
